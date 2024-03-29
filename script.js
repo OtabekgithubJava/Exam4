9647cd4a243420b382a0eaeec1d0784cdae884a8
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2024-03-29 09:47:04 +0500

    Loading...

---
+++ script.js
@@ -0,0 +1,39 @@
+// <!-- <div class="frame-4">
+//     <div class="first-screen bgc">
+
+//     </div>
+//     <div class="right-menu"> -->
+
+//     <!-- </div>
+// </div> -->
+
+// <!-- <div class="mouse-down"></div> -->
+
+// <!-- <div class="scroll-down">Прокрутите вниз</div>
+
+// <div class="group-5">
+//     <div class="rectangle-5"></div>
+//     <div class="ellipse-1"></div>
+// </div> -->
+
+// <!-- <div class="salon-name">Салон красоты «Delote-Beauty» на Крестовском</div> -->
+
+// <!-- <header></header>
+
+// <div class="omega-sign">
+//     <div class="vector"></div>
+// </div> -->
+
+// <!-- <div class="frame-1"> -->
+//     <!-- <div class="home">Главная</div>
+//     <div class="masters">Мастера</div>
+//     <div class="cosmetics">Косметика</div> -->
+// <!-- </div> -->
+// <!-- 
+// <div class="frame-2">
+//     <div class="sales">Акции</div>
+//     <div class="reviews">Отзывы</div>
+//     <div class="contacts">Контакты</div>
+// </div> -->
+
+// <!-- <div class="line-1"></div> -->
